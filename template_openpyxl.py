acae68c80870c271ed13bccba4a29846b030ba2f
Gustavo Santos <gustavosantanadossantos92@gmail.com>
2022-11-30 12:50:54 -0300

    Ajuste comentário

--- template_openpyxl.py
+++ template_openpyxl.py
@@ -17,17 +17,16 @@ print("Ultima Coluna aba 1: " + str(last_col_aba1))
 
 #ler valor de celula em loop
 for i in range(2, last_row_aba1):
-    valor_celula = wb.worksheets[0].cell(row=i,column=1).value
+    valor_celula = wb.worksheets[0].cell(row=i, column=1).value
     print(valor_celula)
 
 #escreve valor na primeira linha disponível na coluna A
 wb.worksheets[0].cell(row=last_row_aba1 + 1, column=1).value = "teste"
 
-#transfere valores da primeira aba para a segunda, loop dentro de loop
+#transfere valores da primeira aba para a segunda
 last_row_aba2 = wb.worksheets[1].max_row
 for i in range(1, last_row_aba1):
         wb.worksheets[1].cell(row=i, column=1).value = wb.worksheets[0].cell(row=i, column=1).value
 
 #salva planilha
 wb.save('template.xlsx')
-
